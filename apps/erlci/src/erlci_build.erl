edae64a4bfd69b04094f956240d3322e7b38f5e7
Marcelo Gornstein <marcelog@gmail.com>
2017-09-14 13:12:30 -0300

    creating build dir

--- apps/erlci/src/erlci_build.erl
+++ apps/erlci/src/erlci_build.erl
@@ -27,18 +27,24 @@
 %%% Includes.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 -include("include/erlci.hrl").
+-include_lib("kernel/include/file.hrl").
+-import_record_info([{file, file_info}]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Exports.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
--export([new/2]).
+-export([new/1]).
+-export([create_build_directory/1]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Public API.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% @doc Creates a new build.
--spec new(erlci_job(), erlci_directory()) -> ok.
-new(Job, Home) ->
+-spec new(erlci_job()) -> ok.
+new(Job) ->
+  #{home := JobHome} = Job,
+  NextBuild = erlci_job:next_build_number(Job),
+  Home = filename:join([JobHome, integer_to_list(NextBuild)]),
   #{
     job => Job,
     home => Home,
@@ -46,3 +52,29 @@ new(Job, Home) ->
     result => in_progress
   }.
 
+-spec create_build_directory(erlci_build()) -> ok.
+create_build_directory(Build) ->
+  #{home := BuildHome} = Build,
+  ok = create_dir(BuildHome).
+
+-spec create_dir(erlci_directory()) -> ok.
+create_dir(Path) ->
+  [First|Components] = filename:split(Path),
+  ok = create_dir(First, Components).
+
+create_dir(Path, []) ->
+  ok = test_and_create_dir(Path);
+
+create_dir(Path, [Next|Components]) ->
+  ok = test_and_create_dir(Path),
+  create_dir(filename:join([Path, Next]), Components).
+
+-spec test_and_create_dir(erlci_directory()) -> ok | not_a_dir.
+test_and_create_dir(Path) ->
+  case file:read_file_info(Path) of
+    {ok, FileInfo} -> case FileInfo#file_info.type of
+      directory -> ok;
+      _ -> not_a_dir
+    end;
+    _ -> ok = file:make_dir(Path)
+  end.
